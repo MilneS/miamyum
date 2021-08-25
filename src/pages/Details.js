8a0713da42dedc86c9b89317c8108196b8708c45
Samira Milne <milne.samira@GMAIL.COM>
2021-08-24 22:32:59 -0500

    fixed style

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -6,8 +6,14 @@ const Details = () => {
       <div className={classes.card}>
         <div className={classes.img}>img</div>
         <div className={classes.itemsContainer}>
-          <div className={classes.title}>Title</div>
-          <div className={classes.description}>description</div>
+          <div className={classes.title}>Title title title</div>
+          <div className={classes.descr}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries.
+          </div>
         </div>
       </div>
     </div>
