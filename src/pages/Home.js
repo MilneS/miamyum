70e832d0ebbe0c2ec0e5df3dda26b614ded03256
Samira Milne <milne.samira@GMAIL.COM>
2021-08-24 21:34:22 -0500

    added responsive home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,13 @@ import classes from "./Home.module.css";
 const Home = () => {
   return (
     <div className={classes.container}>
-      {/* <p className={classes.title}>
+      <p className={classes.title}>
         Welcome to Miamyum's <br />
         photo gallery
       </p>
       <div className={classes.card}>
         <div className={classes.lastPost}>last post</div>
-      </div> */}
+      </div>
     </div>
   );
 };
