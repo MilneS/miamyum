9bdd2ea217a67f30ebf39eea8c59d7820c213b2e
Samira Milne <milne.samira@GMAIL.COM>
2021-08-24 22:18:40 -0500

    added details card

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,17 @@
+import classes from "./Details.module.css";
 
-
-const Details=()=>{
-    return(
-        <div>Details</div>
-    )
-}
+const Details = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.card}>
+        <div className={classes.img}>img</div>
+        <div className={classes.itemsContainer}>
+          <div className={classes.title}>Title</div>
+          <div className={classes.description}>description</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Details;
