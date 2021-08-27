8bfa7dc871ca55e9315ea0cd4991aa303a1d13c2
Samira Milne <milne.samira@GMAIL.COM>
2021-08-26 22:30:40 -0500

    signup firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,46 @@
 import classes from "./Login.module.css";
-import {useDispatch } from "react-redux";
-
+import { useDispatch } from "react-redux";
+// import { useEffect } from "react";
+import { useState } from "react";
 
 const Login = (props) => {
-  const dispatch=useDispatch()
-const signupHandler=()=>{
-  dispatch({type:'showSignup'})
-}
-  
+  const data = {
+    email: "",
+    password: "",
+  };
+  const [enterredData, setEnterredData] = useState(data);
+
+  const enterredDataHandler = (e) => {
+    e.preventDefault();
+    setEnterredData({...enterredData, [e.target.id]: e.target.value})
+  };
+  const formHandler = (e) => {
+    e.preventDefault();
+  };
+  // console.log(enterredData)
+
+  const dispatch = useDispatch();
+  const signupHandler = () => {
+    dispatch({ type: "showSignup" });
+  };
+
   return (
-      <div className={classes.card}>
-        <form className={classes.formContainer}>
-          <div className={classes.title}>Login</div>
-          <div className={classes.errMsg}>Incorrect email or password.</div>
-          <label htmlFor="email" />
-          <input id="email" type="email" placeholder="Email" />
-          <label htmlFor="password" />
-          <input id="password" type="password" placeholder="Password" />
-          <div className={classes.bottomContainer}>
-            <div className={classes.createAccount} onClick={signupHandler}>or create an account.</div>
-            <button className={classes.buttons}>Login</button>
+    <div className={classes.card}>
+      <form onSubmit={formHandler} className={classes.formContainer}>
+        <div className={classes.title}>Login</div>
+        <div className={classes.errMsg}>Incorrect email or password.</div>
+        <label htmlFor="email" />
+        <input id="email" type="email" placeholder="Email" onChange={enterredDataHandler}/>
+        <label htmlFor="password" />
+        <input id="password" type="password" placeholder="Password" onChange={enterredDataHandler}/>
+        <div className={classes.bottomContainer}>
+          <div className={classes.createAccount} onClick={signupHandler}>
+            or create an account.
           </div>
-        </form>
-      </div>
+          <button type='submit' className={classes.buttons}>Login</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,69 @@
 import classes from "./SignUp.module.css";
+import { useState} from "react";
 
 const SignUp = (props) => {
+  const data = {
+    username: "",
+    email: "",
+    password: "",
+  };
+  const [enterredData, setEnterredData] = useState(data);
+
+  const enterredDataHandler = (e) => {
+    e.preventDefault();
+    setEnterredData({ ...enterredData, [e.target.id]: e.target.value });
+  };
+  const formHandler = (e) => {
+    e.preventDefault();
+
+    const enterredUsername = enterredData.username;
+    const enterredEmail = enterredData.email;
+    const enterredPassword = enterredData.password;
+
+    fetch(process.env.REACT_APP_SIGNUP_API, {
+      method: "POST",
+      body: JSON.stringify({
+        username: enterredUsername,
+        email: enterredEmail,
+        password: enterredPassword,
+        returnSecureToken: true,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => {
+      if (res.ok) {
+        //
+      } else {
+        return res.json().then((data) => {
+          console.log(data);
+        });
+      }
+    });
+  };
   return (
     <div className={classes.card}>
-      <form className={classes.formContainer}>
+      <form onSubmit={formHandler} className={classes.formContainer}>
         <div className={classes.title}>Sign Up</div>
         <label htmlFor="username" />
-        <input id="username" type="text" placeholder="Username" />
+        <input
+          id="username"
+          type="text"
+          placeholder="Username"
+          onChange={enterredDataHandler}
+        />
         <label htmlFor="email" />
-        <input id="email" type="email" placeholder="Email" />
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          onChange={enterredDataHandler}
+        />
         <label htmlFor="password" />
-        <input id="password" type="password" placeholder="Password" />
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          onChange={enterredDataHandler}
+        />
         <div className={classes.bottomContainer}>
           <div className={classes.buttondiv}></div>
           <button className={classes.buttons}>Sign Up</button>
