d11c710e8e5edff79c4cd55258830dc724b36830
Samira Milne <milne.samira@GMAIL.COM>
2021-08-29 14:32:01 -0500

    foodData

--- src/components/foodData.js
+++ src/components/foodData.js
@@ -21,3 +21,127 @@ import T from "./foodpics/t.jpg";
 import U from "./foodpics/u.jpg";
 import V from "./foodpics/v.jpg";
 import W from "./foodpics/w.jpg";
+
+const foodData = [
+  {
+    title: "Fraisier",
+    img: A,
+    description:
+      "A classic in French pastry, the fraisier is made with strawberries, génoise and cream.",
+  },
+  {
+    title: "Eclair glacé vanille",
+    img: B,
+    description:
+      "An éclair is an oblong pastry made with choux dough filled with a cream and topped with flavored icing.",
+  },
+  {
+    title: "Cupcake amandes et crème de cheesecake",
+    img: C,
+    description:
+      "almond cupcake topped with cheesecake frosting and chocolate m&m's.",
+  },
+  {
+    title: "Bûche framboise-pistache",
+    img: D,
+    description:
+      "raspberries and pistachio log topped with raspberries and pistachio macarons.",
+  },
+  {
+    title: "Tartelette aux pommes",
+    img: E,
+    description: "French apple tartlet (meaning, no cinnamon!).",
+  },
+  {
+    title: "Cookies au Daim",
+    img: F,
+    description: "Cokkies made with Daim chocolate candy.",
+  },
+  {
+    title: "Paris-Brest",
+    img: G,
+    description:
+      "The wheel shaped pastry, was created to commemorate the Paris–Brest–Paris bicycle race.",
+  },
+  {
+    title: "Mille-feuille",
+    img: H,
+    description: "A mille-feuille is made up of three layers of puff pastry and glazed with icing and chocolate stripes.",
+  },
+  {
+    title: "Coulant au chocolat",
+    img: I,
+    description: "Chocolate lava cake.",
+  },
+  {
+    title: "Tarte aux fraises",
+    img: J,
+    description: "Straberry tart.",
+  },
+  {
+    title: "Gauffres aux fruits",
+    img: K,
+    description: "Fruit waffle.",
+  },
+  {
+    title: "Cakepop vanille",
+    img: L,
+    description: "Vanilla cakepop",
+  },
+  {
+    title: "Eclair craquelin vanille",
+    img: M,
+    description: "Vanilla eclair topped with craquelin.",
+  },
+  {
+    title: "Confiture pomme-banane",
+    img: N,
+    description: "Chunky apple and banana jam",
+  },
+  {
+    title: "Praliné",
+    img: O,
+    description: "A spread made with caramelized roasted almonds and hazelnuts.",
+  },
+  {
+    title: "Pain perdu roulé au Nutella",
+    img: P,
+    description: "Rolled Nutella french toasts.",
+  },
+  {
+    title: "Donuts",
+    img: Q,
+    description: "Well, donuts.",
+  },
+  {
+    title: "Gateau à l'ananas",
+    img: R,
+    description: "Rustic pineapple cake.",
+  },
+  {
+    title: "Cookies aux pépites de chocolat",
+    img: S,
+    description: "Classic chocolate chip cookies.",
+  },
+  {
+    title: "Gateau glacé pistache-franboise",
+    img: T,
+    description: "Iced raspberry and pistachio cake.",
+  },
+  {
+    title: "Cupcake au Nutella",
+    img: U,
+    description: "Nutella cupcake.",
+  },
+  {
+    title: "Milkshake saveur cookies",
+    img: V,
+    description: "Cookie flavored milkshake.",
+  },
+  {
+    title: "Tartelette framboise-nougat",
+    img: W,
+    description: "Raspberry and nougat tartlet.",
+  },
+];
+export default foodData;
