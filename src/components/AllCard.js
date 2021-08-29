75f1bd62659ceb889ed8b6418f1bc0d8e9d5e070
Samira Milne <milne.samira@GMAIL.COM>
2021-08-29 18:27:43 -0500

    overlay on hover

--- src/components/AllCard.js
+++ src/components/AllCard.js
@@ -3,12 +3,12 @@ import foodData from './foodData';
 
 
 const AllCard=(props)=>{
-  const{title,link,description}=props
+  const{title, link, description}=props
     return(
-
         <div className={classes.container}>
         <div className={classes.card}>
           <img className={classes.allimg} src={link} alt=''/>
+          <div className={classes.after}></div>
         </div>
       </div>
     )
