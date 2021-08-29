30445f4ad601dad3dc484477f8e917a33f6ac306
Samira Milne <milne.samira@GMAIL.COM>
2021-08-29 14:33:28 -0500

    all foodData cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import classes from "./Home.module.css";
+import foodData from "../components/foodData";
 
 
 const Home = () => {
+  const pic= foodData[2].img
   return (
     <div className={classes.container}>
       <p className={classes.title}>
@@ -10,6 +12,7 @@ const Home = () => {
       </p>
       <div className={classes.card}>
         <div className={classes.lastPost}>
+          <img className={classes.picstyle} src={pic} alt=''/>
         </div>
       </div>
     </div>
