431821e70e19987744789fc3b08ef3d80fcda714
Samira Milne <milne.samira@GMAIL.COM>
2021-08-29 19:45:59 -0500

    params item

--- src/components/AllCard.js
+++ src/components/AllCard.js
@@ -1,16 +1,11 @@
 import classes from "./AllCard.module.css";
-import { useDispatch } from "react-redux";
 
 const AllCard = (props) => {
   const link=props.link;
-  const item=props.item
 
-  const dispatch=useDispatch();
-const cardHandler=(e)=>{
-  dispatch({type: 'updateItem', item: item})
-}
+
   return (
-    <div className={classes.container} onClick={cardHandler}>
+    <div className={classes.container} >
       <div className={classes.card}>
         <img className={classes.allimg} src={link} alt="" />
           <div className={classes.after} />
--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,31 @@
 import { createStore } from "redux";
 
-const initialState={loggedin: !!localStorage.getItem('token'), showLoginComp:false, showSignupComp:false, idToken:localStorage.getItem('token'), itemData:{}}
-const loginReducer = (state = initialState , action) => {
+const initialState = {
+  loggedin: !!localStorage.getItem("token"),
+  showLoginComp: false,
+  showSignupComp: false,
+  idToken: localStorage.getItem("token"),
+};
+const loginReducer = (state = initialState, action) => {
   if (action.type === "login") {
-    return {...state, loggedin: true };
+    return { ...state, loggedin: true };
   }
   if (action.type === "getToken") {
-    return {...state, idToken: action.token};
+    return { ...state, idToken: action.token };
   }
-  if(action.type==="logout"){
-      return{...state, loggedin: false}
+  if (action.type === "logout") {
+    return { ...state, loggedin: false };
   }
-  if(action.type=== 'showLogin'){
-      return{ ...state, showLoginComp:true, showSignupComp:false}
+  if (action.type === "showLogin") {
+    return { ...state, showLoginComp: true, showSignupComp: false };
   }
-  if(action.type=== 'showSignup'){
-      return{ ...state, showLoginComp:false, showSignupComp:true}
+  if (action.type === "showSignup") {
+    return { ...state, showLoginComp: false, showSignupComp: true };
   }
-  if(action.type==='close'){
-    return{...state, showLoginComp:false, showSignupComp:false}
+  if (action.type === "close") {
+    return { ...state, showLoginComp: false, showSignupComp: false };
   }
-  if(action.type==='updateItem'){
-    return{...state, itemData: action.item}
-  }
-  return state
+  return state;
 };
 
 const store = createStore(loginReducer);
