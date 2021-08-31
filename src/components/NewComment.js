5d5b11d1a609a2909abc45f729cda19a87f62abc
Samira Milne <milne.samira@GMAIL.COM>
2021-08-30 23:11:01 -0500

    api add comments

--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,15 +1,18 @@
-import classes from './NewComment.module.css'
+import classes from "./NewComment.module.css";
 
-const NewComment=()=>{
-    return(
-        <div className={classes.comment}>
-            <form>
-                <label/>
-                <textarea placeholder='Your comment' type='text' maxLength='145'/>
-                <button className={classes.formButton}>Submit</button>
-            </form>
-        </div>
-    )
-}
-export default NewComment
+const NewComment = () => {
+  const commentFunc = async () => {
+      const response= fetch(process.env.REACT_APP_ADD_COMMENTS_API)
+  };
 
+  return (
+    <div className={classes.comment}>
+      <form>
+        <label />
+        <textarea placeholder="Your comment" type="text" maxLength="145" />
+        <button className={classes.formButton}>Submit</button>
+      </form>
+    </div>
+  );
+};
+export default NewComment;
