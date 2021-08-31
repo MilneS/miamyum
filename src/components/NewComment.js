31f272c8cd9bbbb513d72a79ea0c011c5968a516
Samira Milne <milne.samira@GMAIL.COM>
2021-08-30 23:19:36 -0500

    enterredData handler

--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,16 +1,26 @@
 import classes from "./NewComment.module.css";
+import { useState } from "react";
 
 const NewComment = () => {
-  const commentFunc = async () => {
+
+    const [enterredData, setEnterredData] = useState();
+
+    const commentFunc = async () => {
       const response= fetch(process.env.REACT_APP_ADD_COMMENTS_API)
   };
-
+const submitHandler=(e)=>{
+    e.preventDefault()
+    // 
+}
+const enterredDataHandler=(e)=>{
+    setEnterredData(e.target.value)
+}
   return (
     <div className={classes.comment}>
-      <form>
+      <form onSubmit={submitHandler}>
         <label />
-        <textarea placeholder="Your comment" type="text" maxLength="145" />
-        <button className={classes.formButton}>Submit</button>
+        <textarea onChange={enterredDataHandler} name='textarea' placeholder="Your comment" type="text" maxLength="145" />
+        <button type='submit' className={classes.formButton}>Submit</button>
       </form>
     </div>
   );
